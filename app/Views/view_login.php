e5d687af6b9c467bc403fd0c257a5ce8fddb7f55
Yanwar Slamet Priadi <yanwarsp01@gmail.com>
2021-12-15 14:20:17 +0700

    new

---
+++ app/Views/view_login.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="/css/styles.css">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-sm nav-bgcolor">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="">
+        <img class="mx-3" src="/img/logo.png" alt="LAGA Mahasiswa" style="width:120px;">
+      </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link menupage-active mx-5" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menupage mx-5" href="#">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link loginmenu rounded-pill mx-5" href="#">LOG IN</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- body -->
+  <div class="col-md-5 p-lg-5 my-5 textberanda">
+    <h1 class="display-4 font-weight-normal">Maksimalkan Kesempatan Yang Ada Untuk Masa Depanmu</h1>
+    <p class="lead font-weight-normal">
+      <br>
+      <br>
+      <br>
+      LAGA Mahasiswa adalah salah satu cara terbaik kamu untuk mengeksplor diri. Dengan mengikuti kegiatan kemahasiswaan yang ditawarkan.
+    </p>
+    <a class="btn rounded button1" href="#">Cari Programmu</a>
+  </div>
+
+  <!-- footer -->
+  <footer class="py-2 mt-auto">
+    <div class="container-fluid px-4">
+      <div class="d-flex align-items-center justify-content-between small">
+        <div class="text-muted">Copyright &copy; 2021</div>
+        <div class="text-muted">
+          <a href="#">Yanwar Slamet</a>
+          &middot;
+          <a href="#">Zahra Aulia</a>
+          &middot;
+          <a href="#">Raflie Firmansyah</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
