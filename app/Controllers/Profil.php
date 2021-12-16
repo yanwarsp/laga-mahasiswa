95781f64a4bfd970cffcfdb7acc1800a8e607657
Yanwar Slamet Priadi <yanwarsp01@gmail.com>
2021-12-17 02:23:52 +0700

    gadangggggggggg

--- app/Controllers/Profil.php
+++ app/Controllers/Profil.php
@@ -8,6 +8,11 @@ class Profil extends BaseController
 {
     public function index()
     {
-        //
+        echo view("profil/view_profil");
+    }
+
+    public function edit()
+    {
+        echo view("profil/view_editprofil");
     }
 }
---
+++ app/Views/profil/view_editprofil.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="/css/styles.css">
+
+  <title>Beranda</title>
+</head>
+
+<body class="bodycolor">
+
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-sm nav-bgcolor">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="">
+        <img class="mx-3" src="/img/logo.png" alt="LAGA Mahasiswa" style="width:120px;">
+      </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/magang">Magang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/lomba">Lomba</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/about">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link loginmenu rounded-pill mx-5" href="<?= base_url(); ?>/login">LOG IN</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- body -->
+
+
+  <!-- footer -->
+  <footer class="py-2">
+    <div class="container-fluid px-4">
+      <div class="align-items-center d-flex small">
+        <div class="text-muted col-sm">
+          <img src="/img/logo2.png" alt="LAGA Mahasiswa" width="9%">
+          &nbsp; Copyright &copy; 2021
+        </div>
+        <div class="text-muted">
+          <a href="#" class="text-muted textputih">Yanwar</a>
+          &middot;
+          <a href="#" class="text-muted textputih">Zahra</a>
+          &middot;
+          <a href="#" class="text-muted textputih">Raflie</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
