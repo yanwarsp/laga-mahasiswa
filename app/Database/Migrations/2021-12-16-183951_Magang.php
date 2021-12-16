696d2d8fd7be858cfed78d4f847805be2f5a1416
Yanwar Slamet Priadi <yanwarsp01@gmail.com>
2021-12-17 02:04:25 +0700

    gadangmulujadikagag

--- app/Database/Migrations/2021-12-16-183951_Magang.php
+++ app/Database/Migrations/2021-12-16-183951_Magang.php
@@ -8,11 +8,46 @@ class Magang extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'namalengkap' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'prodi' => [
+                'type' => 'VARCHAR',
+                'constraint' => 40,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 60,
+            ],
+            'foto' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => TRUE,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => TRUE,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('username', true);
+        $this->forge->createTable('magang');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('magang');
     }
 }
