e5f1ff8f33f45fc9dc7080172285da8c452e5cca
Yanwar Slamet Priadi <yanwarsp01@gmail.com>
2021-12-17 13:03:46 +0700

    5 jam lagi ges

--- app/Views/profil/view_editprofil.php
+++ app/Views/profil/view_editprofil.php
@@ -2,181 +2,179 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="/css/styles.css">
+	<link rel="stylesheet" href="/css/styles.css">
 
-  <title>Beranda</title>
+	<title>Beranda</title>
 </head>
 
 <body class="bodycolor">
 
-  <!-- navbar -->
-  <nav class="navbar navbar-expand-sm nav-bgcolor">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="">
-        <img class="mx-3" src="/img/logo.png" alt="LAGA Mahasiswa" style="width:120px;">
-      </a>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/">Beranda</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/magang">Magang</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/lomba">Lomba</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link menupage mx-5" href="<?= base_url(); ?>/about">Tentang Kami</a>
-        </li>
-        <?php if (!session()->get('logged_in')) : ?>
-          <li class="nav-item">
-            <a class="nav-link loginmenu rounded-pill mx-5" href="<?= base_url(); ?>/login">LOG IN</a>
-          </li>
-        <?php endif; ?>
-        <?php if (session()->get('logged_in')) : ?>
-          <li class="nav-item">
-            <a class="nav-link loginmenu rounded-pill mx-5" href="<?= base_url(); ?>/profil"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
-                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
-              </svg></a>
-          </li>
-        <?php endif; ?>
-      </ul>
-    </div>
-  </nav>
+	<!-- navbar -->
+	<nav class="navbar navbar-expand-sm nav-bgcolor">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="">
+				<img class="mx-3" src="/img/logo.png" alt="LAGA Mahasiswa" style="width:120px;">
+			</a>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link menupage mx-5" href="<?= base_url(); ?>/">Beranda</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link menupage mx-5" href="<?= base_url(); ?>/magang">Magang</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link menupage mx-5" href="<?= base_url(); ?>/lomba">Lomba</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link menupage mx-5" href="<?= base_url(); ?>/about">Tentang Kami</a>
+				</li>
+				<?php if (!session()->get('logged_in')) : ?>
+					<li class="nav-item">
+						<a class="nav-link loginmenu rounded-pill mx-5" href="<?= base_url(); ?>/login">LOG IN</a>
+					</li>
+				<?php endif; ?>
+				<?php if (session()->get('logged_in')) : ?>
+					<li class="nav-item">
+						<a class="nav-link loginmenu rounded-pill mx-5" href="<?= base_url(); ?>/profil"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+								<path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+							</svg></a>
+					</li>
+				<?php endif; ?>
+			</ul>
+		</div>
+	</nav>
 
-  <!-- body -->
-  <div class="container">
-<div class="row gutters">
-<div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
-<div class="card h-100">
-	<div class="card-body">
-		<div class="account-settings">
-			<div class="user-profile">
-				<div class="user-avatar">
-					<img src="img/" alt="Maxwell Admin">
+	<!-- body -->
+	<div class="container mb-5 mt-5">
+		<div class="row gutters">
+			<div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+				<div class="card h-100">
+					<div class="card-body">
+						<div class="account-settings">
+							<div class="user-profile">
+								<div class="user-avatar">
+									<img src="/img/profil_default.png" alt="" height="200px" />
+								</div> <br>
+								<h4 class="user-name text-center">Melinda Rahayu</h4>
+							</div>
+
+						</div>
+					</div>
 				</div>
-				<h5 class="user-name">Yuki Hayashi</h5>
-				<h6 class="user-email">yuki@Maxwell.com</h6>
 			</div>
-			<div class="about">
-				<h5>About</h5>
-				<p>I'm Yuki. Full Stack Designer I enjoy creating user-centric, delightful and human experiences.</p>
+			<div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+				<div class="card h-100">
+					<div class="card-body">
+						<div class="row gutters">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+								<h6 class="mt-3 mb-3">Edit Profile</h6>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="fullName">Full Name</label>
+									<input type="text" class="form-control" id="fullName" placeholder="Enter full name">
+								</div>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="eMail">Email</label>
+									<input type="email" class="form-control" id="eMail" placeholder="Enter email ID">
+								</div>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="phone">Phone</label>
+									<input type="text" class="form-control" id="phone" placeholder="Enter phone number">
+								</div>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="website">Website URL</label>
+									<input type="url" class="form-control" id="website" placeholder="Website url">
+								</div>
+							</div>
+						</div>
+						<div class="row gutters">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+								<h6 class="mt-3 mb-2 text-primary">Address</h6>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="Street">Street</label>
+									<input type="name" class="form-control" id="Street" placeholder="Enter Street">
+								</div>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="ciTy">City</label>
+									<input type="name" class="form-control" id="ciTy" placeholder="Enter City">
+								</div>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="sTate">State</label>
+									<input type="text" class="form-control" id="sTate" placeholder="Enter State">
+								</div>
+							</div>
+							<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+								<div class="form-group">
+									<label for="zIp">Zip Code</label>
+									<input type="text" class="form-control" id="zIp" placeholder="Zip Code">
+								</div>
+							</div>
+						</div>
+						<div class="row gutters">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+								<div class="text-right">
+									<br>
+									<br>
+									<button type="button" id="submit" name="submit" class="btn btn-secondary">Cancel</button>
+									<button type="button" id="submit" name="submit" class="btn btn-primary">Update</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-</div>
-<div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
-<div class="card h-100">
-	<div class="card-body">
-		<div class="row gutters">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<h6 class="mb-2 text-primary">Personal Details</h6>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="fullName">Full Name</label>
-					<input type="text" class="form-control" id="fullName" placeholder="Enter full name">
+
+
+
+	<!-- footer -->
+	<footer class="py-2">
+		<div class="container-fluid px-4">
+			<div class="align-items-center d-flex small">
+				<div class="text-muted col-sm">
+					<img src="/img/logo2.png" alt="LAGA Mahasiswa" width="9%">
+					&nbsp; Copyright &copy; 2021
 				</div>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="eMail">Email</label>
-					<input type="email" class="form-control" id="eMail" placeholder="Enter email ID">
-				</div>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="phone">Phone</label>
-					<input type="text" class="form-control" id="phone" placeholder="Enter phone number">
-				</div>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="website">Website URL</label>
-					<input type="url" class="form-control" id="website" placeholder="Website url">
+				<div class="text-muted">
+					<a href="https://github.com/yanwarsp" class="text-muted textputih">Yanwar</a>
+					&middot;
+					<a href="https://github.com/Zahraauliafauzia" class="text-muted textputih">Zahra</a>
+					&middot;
+					<a href="https://github.com/Raflie26" class="text-muted textputih">Raflie</a>
 				</div>
 			</div>
 		</div>
-		<div class="row gutters">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<h6 class="mt-3 mb-2 text-primary">Address</h6>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="Street">Street</label>
-					<input type="name" class="form-control" id="Street" placeholder="Enter Street">
-				</div>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="ciTy">City</label>
-					<input type="name" class="form-control" id="ciTy" placeholder="Enter City">
-				</div>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="sTate">State</label>
-					<input type="text" class="form-control" id="sTate" placeholder="Enter State">
-				</div>
-			</div>
-			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="form-group">
-					<label for="zIp">Zip Code</label>
-					<input type="text" class="form-control" id="zIp" placeholder="Zip Code">
-				</div>
-			</div>
-		</div>
-		<div class="row gutters">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<div class="text-right">
-					<button type="button" id="submit" name="submit" class="btn btn-secondary">Cancel</button>
-					<button type="button" id="submit" name="submit" class="btn btn-primary">Update</button>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
-</div>
-</div>
+	</footer>
 
 
+	<!-- Optional JavaScript; choose one of the two! -->
 
-  <!-- footer -->
-  <footer class="py-2">
-    <div class="container-fluid px-4">
-      <div class="align-items-center d-flex small">
-        <div class="text-muted col-sm">
-          <img src="/img/logo2.png" alt="LAGA Mahasiswa" width="9%">
-          &nbsp; Copyright &copy; 2021
-        </div>
-        <div class="text-muted">
-          <a href="https://github.com/yanwarsp" class="text-muted textputih">Yanwar</a>
-          &middot;
-          <a href="https://github.com/Zahraauliafauzia" class="text-muted textputih">Zahra</a>
-          &middot;
-          <a href="https://github.com/Raflie26" class="text-muted textputih">Raflie</a>
-        </div>
-      </div>
-    </div>
-  </footer>
+	<!-- Option 1: Bootstrap Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-
-  <!-- Optional JavaScript; choose one of the two! -->
-
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
+	<!-- Option 2: Separate Popper and Bootstrap JS -->
+	<!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
